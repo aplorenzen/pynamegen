a64d6bc58cb71a89069ae869fa50c5542950fbe2
Andreas Lorenzen <aplorenzen@gmail.com>
2019-03-23 10:39:40 +0100

    Added access control header, allow all

--- namegenserver.py
+++ namegenserver.py
@@ -38,20 +38,21 @@ class NameServer(BaseHTTPRequestHandler):
                             count = int(query.get('count')[0])
 
                 response_obj = []
-                for x in range(0, count):
+                for _ in range(0, count):
                     first_name = NameServer.first_names[random.randint(0, len(NameServer.first_names) - 1)]
                     last_name = NameServer.last_names[random.randint(0, len(NameServer.last_names) - 1)]
                     name_obj = {'name': first_name + " " + last_name}
                     response_obj.append(name_obj)
                 self.send_response(200)
             else:
-                response_obj = {'error': "The path " + path + " is not valid. please try /name"};
+                response_obj = {'error': "The path " + path + " is not valid. please try /name"}
                 self.send_response(404)
         except BaseException as be:
             response_obj = {'error': "Exception message: " + str(be)}
             print(traceback.format_exc())
             self.send_response(500)
 
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Content-Type', 'application/json')
         self.end_headers()
         self.wfile.write(bytes(json.dumps(response_obj), 'utf-8'))
